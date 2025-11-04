2aa8fd4cacb33319337ef44cf9e324f86de83368
zgo <zero.li.zhh@gmail.com>
2025-11-04 14:03:13 +0800

    refactor(android): 更新Flutter资源加载API
    - 将FlutterMain替换为FlutterLoader以适配新版本
    - 更新BitmapDescriptorFactory中资源获取方式

--- android/src/main/java/com/amap/flutter/map/utils/ConvertUtil.java
+++ android/src/main/java/com/amap/flutter/map/utils/ConvertUtil.java
@@ -30,7 +30,7 @@ import java.util.Map;
 
 import androidx.annotation.NonNull;
 
-import io.flutter.view.FlutterMain;
+import io.flutter.embedding.engine.loader.FlutterLoader;   // ← 新 API
 
 /**
  * @author whm
@@ -373,15 +373,16 @@ public class ConvertUtil {
             case "fromAsset":
                 if (data.size() == 2) {
                     return BitmapDescriptorFactory.fromAsset(
-                            FlutterMain.getLookupKeyForAsset(toString(data.get(1))));
+                            new FlutterLoader().getLookupKeyForAsset(toString(data.get(1))));   // ← 替换
                 } else {
                     return BitmapDescriptorFactory.fromAsset(
-                            FlutterMain.getLookupKeyForAsset(toString(data.get(1)), toString(data.get(2))));
+                            new FlutterLoader().getLookupKeyForAsset(toString(data.get(1)),
+                                    toString(data.get(2))));                                   // ← 替换
                 }
             case "fromAssetImage":
                 if (data.size() == 3) {
                     return BitmapDescriptorFactory.fromAsset(
-                            FlutterMain.getLookupKeyForAsset(toString(data.get(1))));
+                            new FlutterLoader().getLookupKeyForAsset(toString(data.get(1))));   // ← 替换
                 } else {
                     throw new IllegalArgumentException(
                             "'fromAssetImage' Expected exactly 3 arguments, got: " + data.size());
